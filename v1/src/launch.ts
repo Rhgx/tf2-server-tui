76cc85ea69496959b3f274c4e8218bef33867638
Rhgx <eren.serbetcioglu@gmail.com>
2026-04-20 01:38:40 +0300

    Remove V1

--- v1/src/launch.ts
+++
@@ -1,6 +0,0 @@
-import open from 'open';
-
-export async function launchTF2(ip: string, port: number): Promise<void> {
-    const connectUrl = `steam://connect/${ip}:${port}`;
-    await open(connectUrl);
-}
